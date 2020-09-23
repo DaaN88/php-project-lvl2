6ba11f51ededc5d58bd00178b7e27744127291c1
DaaN88 <ant-shvedov@yandex.ru>
2020-09-23 15:11:58 +0400

    second step - added --format

--- src/dataForGendiff.php
+++ src/dataForGendiff.php
@@ -10,9 +10,11 @@ function getReference()
     Usage:
     gendiff (-h|--help)
     gendiff (-v|--version)
+    gendiff [--format <fmt>] <firstFile> <secondFile>
     
     Options:
         -h --help                     Show this screen
         -v --version                  Show version
+        --format <fmt>                Report format [default: stylish]
     DOCOPT;
 }
